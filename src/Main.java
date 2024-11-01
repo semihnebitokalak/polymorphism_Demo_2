28bdc9424acfbfd499bb17a43dfd289008c04918
GitHub Kullanıcı Adınız <GitHub E-posta Adresiniz>
2024-11-01 17:38:47 +0300

    Polymorphism örnekleri

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        animal.sound();
+        Animal animal2 = new Dog();
+        animal2.sound();
+        Animal animal3 = new Cat();
+        animal3.sound();
+
+    }
+}
